b657732b05c98f12670f585162f1c8efe42577cb
demfels <arnott.daniel@gmail.com>
2014-03-08 10:46:07 -0600

    Added a test java file MainTest.java
    Added this file along with the eclipse project for our class.

---
+++ src/edu/stthomas/seis635/MainTest.java
@@ -0,0 +1,13 @@
+package edu.stthomas.seis635;
+
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
